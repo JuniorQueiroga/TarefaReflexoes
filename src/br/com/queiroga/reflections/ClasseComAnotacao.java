a9a02b8ff9f9b0f767d313db75dd637a41ef53ae
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-10-31 17:50:19 -0300

    Update ClasseComAnotacao.java

--- src/br/com/queiroga/reflections/ClasseComAnotacao.java
+++ src/br/com/queiroga/reflections/ClasseComAnotacao.java
@@ -1,4 +1,9 @@
 package br.com.queiroga.reflections;
 
 public class ClasseComAnotacao {
+    @AnotacaoTabela("Tabela2")
+    private String nomeTabela;
+
+    public ClasseComAnotacao() {
+    }
 }

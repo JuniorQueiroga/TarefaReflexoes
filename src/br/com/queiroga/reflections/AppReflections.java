a1184ca66244d54b4a50829e07086dc05a998a39
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-10-31 17:49:51 -0300

    Update AppReflections.java

--- src/br/com/queiroga/reflections/AppReflections.java
+++ src/br/com/queiroga/reflections/AppReflections.java
@@ -1,4 +1,12 @@
 package br.com.queiroga.reflections;
 
 public class AppReflections {
+    public AppReflections() {
+    }
+
+    public static void main(String[] args) {
+        Class clazz = Produto.class;
+        System.out.println(clazz);
+        System.out.println(AnotacaoTabela.class);
+    }
 }

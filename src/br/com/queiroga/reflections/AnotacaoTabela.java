7d8a36b70544fbd24ff0e4984d503bd5a36e1c4c
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-10-31 17:49:20 -0300

    Update AnotacaoTabela.java

--- src/br/com/queiroga/reflections/AnotacaoTabela.java
+++ src/br/com/queiroga/reflections/AnotacaoTabela.java
@@ -1,4 +1,14 @@
 package br.com.queiroga.reflections;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
 public @interface AnotacaoTabela {
+    String value();
 }

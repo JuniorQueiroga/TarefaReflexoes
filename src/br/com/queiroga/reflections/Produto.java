ed84c77f3d7540e4c92ba28a23ee8a98fb3a38a6
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-10-31 17:53:09 -0300

    Update Produto.java

--- src/br/com/queiroga/reflections/Produto.java
+++ src/br/com/queiroga/reflections/Produto.java
@@ -1,4 +1,34 @@
 package br.com.queiroga.reflections;
 
 public class Produto {
+    private Long codigo;
+    private String descricao;
+    private double valor;
+
+    public Produto() {
+    }
+
+    public Long getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
